0f9daaf05a6a42660e1a1416d8c1b68f3117dc8c
Riviann de Andrade <riviann.anddos@educa.jcyl.es>
2026-02-07 23:45:39 +0100

    Se crea el index

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include 'conexion.php';
+
+$sql = "SELECT * FROM personas";
+$result = sqlsrv_query($conn, $sql);
+?>
+
+<h2>Lista de personas</h2>
+
+<table border="1">
+<tr>
+    <th>ID</th>
+    <th>Nombre</th>
+    <th>Edad</th>
+    <th>Email</th>
+</tr>
+
+<?php while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) { ?>
+<tr>
+    <td><?= $row['id'] ?></td>
+    <td><?= $row['nombre'] ?></td>
+    <td><?= $row['edad'] ?></td>
+    <td><?= $row['email'] ?></td>
+</tr>
+<?php } ?>
+</table>
